0de38543675712aebf95e384cd90bbdbc92f1fe7
BenStein1 <copprman@gmail.com>
2023-10-11 20:09:07 -0700

    Refactoring code

--- src/wbg/__main__.py
+++ src/wbg/__main__.py
@@ -1,4 +1,10 @@
-from wbg.app import main
+#from wbg.app import main
+#
+#if __name__ == '__main__':
+#    main().main_loop()
+from wbg.app import WeckterBackstoryGenerator
 
 if __name__ == '__main__':
-    main().main_loop()
+    app_instance = WeckterBackstoryGenerator()
+    app_instance.main()
+    app_instance.main_loop()
